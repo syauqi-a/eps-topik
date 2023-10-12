057602ebc1b52ccded6f4ce8b8f9fabff5b294f0
Syauqi <kahmadsyauqi@upi.edu>
2023-10-12 15:55:44 +0700

    Add User, Role & Permission policies

---
+++ app/Policies/PermissionPolicy.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Permission;
+use App\Models\User;
+
+class PermissionPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->hasRole(['super admin', 'admin']);
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Permission $permission): bool
+    {
+        return (
+            $user->hasRole(['super admin', 'admin']) or
+            $user->hasPermissionTo('view permissions')
+        );
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return (
+            $user->hasRole(['super admin', 'admin']) or
+            $user->hasPermissionTo('create permissions')
+        );
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Permission $permission): bool
+    {
+        return (
+            $user->hasRole(['super admin', 'admin']) or
+            $user->hasPermissionTo('edit permissions')
+        );
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Permission $permission): bool
+    {
+        return (
+            $user->hasRole(['super admin']) or
+            $user->hasPermissionTo('delete permissions')
+        );
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Permission $permission): bool
+    {
+        return (
+            $user->hasRole(['super admin']) or
+            $user->hasPermissionTo('delete permissions')
+        );
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Permission $permission): bool
+    {
+        return (
+            $user->hasRole(['super admin']) or
+            $user->hasPermissionTo('delete permissions')
+        );
+    }
+}
