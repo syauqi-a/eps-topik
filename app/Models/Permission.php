e996c925dc9aea55705ad18e4f43554a87e1d297
Syauqi <kahmadsyauqi@upi.edu>
2023-10-12 08:24:45 +0700

    Add Role & Permission models

---
+++ app/Models/Permission.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Maklad\Permission\Models\Permission as ModelsPermission;
+
+// use Illuminate\Database\Eloquent\Model;
+
+class Permission extends ModelsPermission
+{
+    use HasFactory;
+}
---
+++ app/Models/Role.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Maklad\Permission\Models\Role as ModelsRole;
+
+// use Illuminate\Database\Eloquent\Model;
+
+class Role extends ModelsRole
+{
+    use HasFactory;
+}
