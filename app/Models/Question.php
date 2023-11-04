3e3f2d83fe4f426c0ca13305282d59d105811d3b
Syauqi <kahmadsyauqi@upi.edu>
2023-11-04 14:37:21 +0700

    Add Qeust types & Tags list

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -33,4 +33,26 @@ class Question extends Model
             relation: 'multiple_choices'
         );
     }
+
+    public function scopeQuestionTypes(): array
+    {
+        return [
+            '듣기' => '듣기 (listening)',
+            '읽기' => '읽기 (reading)',
+        ];
+    }
+
+    public function scopeTags(): array
+    {
+        return [
+            '빈칸' => '빈칸 (isi yg kosong)',
+            '내용' => '내용 (isi pokok)',
+            '관계있는' => '관계있는 (yg berkaitan)',
+            '반대말' => '반대말 (antonim)',
+            '비슷말' => '비슷말 (sinonim)',
+            '그림/사진' => '그림/사진 (gambar)',
+            '그래프' => '그래프 (grafik)',
+            '표지판' => '표지판 (rambu-rambu)',
+        ];
+    }
 }
