6108d1ea90f3dce0259735a4e880ebd27e2f43bb
Syauqi <kahmadsyauqi@upi.edu>
2023-11-08 17:00:16 +0700

    Remove index

--- database/migrations/2023_10_30_123501_create_assignments_table.php
+++ database/migrations/2023_10_30_123501_create_assignments_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('assignments', function (Blueprint $collection) {
             $collection->id();
-            $collection->index('name');
             $collection->timestamps();
         });
     }
--- database/migrations/2023_11_03_225839_create_questions_table.php
+++ database/migrations/2023_11_03_225839_create_questions_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('questions', function (Blueprint $collection) {
             $collection->id();
-            $collection->index('name');
             $collection->timestamps();
         });
     }
